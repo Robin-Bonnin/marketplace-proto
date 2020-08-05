2c32c32a9975bbbd9b26ac8f364d8e69130734a7
Robin-Bonnin <62978043+Robin-Bonnin@users.noreply.github.com>
2020-08-05 23:06:53 +0700

    Added grpc modules

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-
+import * as fcl from "@onflow/fcl";
+import * as sdk from "@onflow/sdk";
+import * as types from "@onflow/types";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'marketplace-proto';
+
+  login = () => {
+    fcl.authenticate();
+  }
 }
+
+
